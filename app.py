0aec0519c57e7bacd406e226b4424ff948df0fd7
Amazeryogo <suvid.datta@gmail.com>
2021-10-12 18:09:00 +0530

    i like milk

--- app.py
+++ app.py
@@ -1,7 +1,23 @@
-from flask import *
+from flask import Flask, render_template, flash, redirect
+from forms import *
+import os
+
+
 
 app = Flask(__name__)
+SECRET_KEY = os.urandom(32)
+app.config['SECRET_KEY'] = SECRET_KEY
 
 @app.route('/')
-def home():
-    return render_template("home.html")
+@app.route('/home')
+def index():
+    return render_template('home.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect('/home')
+    return render_template('login.html', title='Sign In', form=form)
