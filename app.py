0f01cc5a8a0698de0a8121aca20cb4dee32f24a8
Amazeryogo <suvid.datta@gmail.com>
2021-10-13 15:04:27 +0530

    new stuff

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, flash, redirect
 from forms import *
 import os
+from db import *
 
 
 
@@ -17,7 +18,27 @@ def index():
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me={}'.format(
-            form.username.data, form.remember_me.data))
+        x = login_check(form.username.data,form.password.data)
+        flash(str(x))
         return redirect('/home')
     return render_template('login.html', title='Sign In', form=form)
+
+@app.route('/create', methods=['GET', 'POST'])
+def create():
+    form = CreateUserForm()
+    if form.validate_on_submit():
+        x = add_user(form.username.data, form.password.data, form.email.data, form.invcode.data)
+        '''if x == "OK":
+            return redirect('/home')
+        else:'''
+        flash(str(x))
+        return redirect('/home')
+    return render_template('create.html', title='Sign In', form=form)
+
+
+
+@app.route('/loggingin')  
+def cookie():  
+    res = make_response("<h1>cookie is set</h1>")  
+    res.set_cookie('foo','bar')  
+    return res  
