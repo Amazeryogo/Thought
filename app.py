949b897230d91e9eaab9c351d2f5f5be63d891c0
Suvid Datta <suvid.datta@gmail.com>
2022-01-17 11:18:37 +0530

    changes

--- app.py
+++ app.py
@@ -1,30 +1,30 @@
-
-from flask import Flask, flash, render_template, request, session, make_response,  redirect, url_for
+from flask import Flask, flash, render_template, request, session, make_response, redirect, url_for
 from forms import *
 import os
-from turbo_flask import Turbo
 from flask_pymongo import *
 from flask_login import LoginManager
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import current_user, login_user, logout_user, login_required, UserMixin
 import uuid
 from hashlib import md5
-from  datetime import datetime as bruh
+from datetime import datetime as bruh
+import flask_bootstrap
 
 appx = Flask(__name__)
 SECRET_KEY = os.urandom(32)
 appx.config['SECRET_KEY'] = SECRET_KEY
-appx.config["MONGO_URI"] = "mongodb://localhost:27017/ThoughtDB" 
+appx.config["MONGO_URI"] = "mongodb://localhost:27017/ThoughtDB"
 mongo = PyMongo(appx)
 login = LoginManager(appx)
 login.login_view = '/login'
 appx.config['TESTING'] = False
 db = mongo.db
-turb = Turbo(appx)
+flask_bootstrap.Bootstrap(appx)
+
 
 class User(UserMixin):
 
-    def __init__(self, username, email, password,invcode, _id=None,aboutme=None):
+    def __init__(self, username, email, password, invcode, _id=None, aboutme=None):
         if aboutme is None:
             aboutme = "New to Thought"
             self.aboutme = aboutme
@@ -35,16 +35,18 @@ class User(UserMixin):
         self.email = email
         self.password = password
         self.invcode = invcode
-        self._id = uuid.uuid4().hex if _id is None else _id 
+        self._id = uuid.uuid4().hex if _id is None else _id
         self.posts = db.postdb.find({"user_id": self._id})
-    
 
     def is_authenticated(self):
         return True
+
     def is_active(self):
         return True
+
     def is_anonymous(self):
         return False
+
     def get_id(self):
         return self._id
 
@@ -53,7 +55,7 @@ class User(UserMixin):
         data = db.userdb.find_one({"username": username})
         if data is not None:
             return cls(**data)
-        
+
     @classmethod
     def addaboutme(cls, username, aboutme):
         db.userdb.update_one({"username": username}, {"$set": {"aboutme": aboutme}})
@@ -79,14 +81,13 @@ class User(UserMixin):
             return cls(**data)
 
     @classmethod
-    def change_email(cls,username,email):
+    def change_email(cls, username, email):
         existing_user = db.userdb.find_one({"email": email})
         if existing_user is None:
             db.userdb.update_one({"username": username}, {"$set": {"email": email}})
             return True
         else:
             return False
-
 
     @staticmethod
     def login_valid(username, password):
@@ -107,19 +108,18 @@ class User(UserMixin):
         else:
             return False
 
-
     def json(self):
         return {
             "username": self.username,
             "email": self.email,
             "_id": self._id,
-            "aboutme":self.aboutme,
-            "invcode" : self.invcode,
+            "aboutme": self.aboutme,
+            "invcode": self.invcode,
             "password": self.password
         }
 
     @classmethod
-    def avatar(self,username):
+    def avatar(self, username):
         x = db.userdb.find_one({"username": username})
         if x is not None:
             email = x["email"]
@@ -128,46 +128,49 @@ class User(UserMixin):
                 digest, 128)
 
     def save_to_mongo(self):
-        db.userdb.insert(self.json())
+        db.userdb.insert_one(self.json())
+
 
 class Messages:
-        def __init__(self, sender, receiver,timestamp,message, _id=None):
-            self.sender = sender
-            self.receiver = receiver
-            self.timestamp = timestamp
-            self.message = message
-            self._id = uuid.uuid4().hex if _id is None else _id
+    def __init__(self, sender, receiver, timestamp, message, _id=None):
+        self.sender = sender
+        self.receiver = receiver
+        self.timestamp = timestamp
+        self.message = message
+        self._id = uuid.uuid4().hex if _id is None else _id
 
-        def json(self):
-            return {
-                "sender": self.sender,
-                "receiver": self.receiver,
-                "message": self.message,
-                "timestamp": self.timestamp,
-                "_id": self._id
-            }
+    def json(self):
+        return {
+            "sender": self.sender,
+            "receiver": self.receiver,
+            "message": self.message,
+            "timestamp": self.timestamp,
+            "_id": self._id
+        }
 
-        # a function that compiles a chat between two users and sort them by timestamps
-        @classmethod
-        def get_chat(cls, user1, user2):
-            chats = db.messagesdb.find({"$or": [{"sender": user1, "receiver": user2}, {"sender": user2, "receiver": user1}]})
-            # sort using timestamps
-            chats = list(chats)
-            chats.sort(key=lambda x: x["timestamp"])
-            return [cls(**chat) for chat in chats]
-        
-        @classmethod
-        def send_message(cls, sender, receiver, message):
-            timestamp = bruh.now()
-            new_message = cls(sender, receiver, timestamp, message)
-            new_message.save_to_mongo()
-            return new_message.json()
+    # a function that compiles a chat between two users and sort them by timestamps
+    @classmethod
+    def get_chat(cls, user1, user2):
+        chats = db.messagesdb.find(
+            {"$or": [{"sender": user1, "receiver": user2}, {"sender": user2, "receiver": user1}]})
+        # sort using timestamps
+        chats = list(chats)
+        chats.sort(key=lambda x: x["timestamp"])
+        return [cls(**chat) for chat in chats]
 
-        def save_to_mongo(self):
-            db.messagesdb.insert(self.json())
-       
+    @classmethod
+    def send_message(cls, sender, receiver, message):
+        timestamp = bruh.now()
+        new_message = cls(sender, receiver, timestamp, message)
+        new_message.save_to_mongo()
+        return new_message.json()
+
+    def save_to_mongo(self):
+        db.messagesdb.insert_one(self.json())
+
+
 class Post:
-    def __init__(self, username,title, content,timestamp, user_id, _id=None):
+    def __init__(self, username, title, content, timestamp, user_id, _id=None):
         self.title = title
         self.content = content
         self.user_id = user_id
@@ -200,9 +203,11 @@ class Post:
     def save_to_mongo(self):
         db.postdb.insert_one(self.json())
 
+
 @login.user_loader
 def load_user(user_id):
     return User.get_by_id(user_id)
+
 
 # render latest posts
 @appx.route('/')
@@ -210,6 +215,7 @@ def load_user(user_id):
 def home():
     posts = db.postdb.find().sort("timestamp", DESCENDING).limit(10)
     return render_template('home.html', posts=posts)
+
 
 @appx.route("/register", methods=['GET', 'POST'])
 def register():
@@ -219,10 +225,10 @@ def register():
             username = request.form["username"]
             email = request.form["email"]
             invcode = request.form["invcode"]
-            password = generate_password_hash(request.form["password"])#.decode('utf-8')
-            find_user =  User.get_by_email(email)
+            password = generate_password_hash(request.form["password"])  # .decode('utf-8')
+            find_user = User.get_by_email(email)
             if find_user is None:
-                User.register(username, email, password,invcode)
+                User.register(username, email, password, invcode)
                 flash(f'Account created for {form.username.data}!', 'success')
                 return redirect(url_for('home'))
             else:
@@ -243,7 +249,7 @@ def login():
                 current_user.is_authenticated = True
                 flash(f'You are now logged in as {form.username.data}!', 'success')
                 return redirect('/me')
-                
+
             else:
                 flash(f'Invalid login!', 'danger')
     return render_template('login.html', title='Login', form=form)
@@ -258,10 +264,11 @@ def logout():
 @appx.route('/api/about')
 def api_about():
     uhh = {
-        "name" : "Thought",
+        "name": "Thought",
         "version": "1.0"
     }
     return str(uhh)
+
 
 @appx.route("/api/register", methods=['GET', 'POST'])
 def registerapi():
@@ -269,13 +276,14 @@ def registerapi():
     username = x.get("username")
     email = x.get("email")
     invcode = x.get("invcode")
-    password = generate_password_hash(x.get("password"))#.decode('utf-8')
-    find_user =  User.get_by_email(email)
+    password = generate_password_hash(x.get("password"))  # .decode('utf-8')
+    find_user = User.get_by_email(email)
     if find_user is None:
-        User.register(username, email, password,invcode)
-        return "{\"comment\" : \"OK!\"}" 
+        User.register(username, email, password, invcode)
+        return "{\"comment\" : \"OK!\"}"
     else:
         return "{\"comment\" : \"USER ALREADY EXISTS, ERROR 404\"}"
+
 
 @appx.route("/api/login", methods=['GET', 'POST'])
 def loginapi():
@@ -284,10 +292,10 @@ def loginapi():
     password = x.get("password")
     user = User.get_by_username(username)
     if user is not None and User.login_valid(username, password):
-        return "{\"comment\" : \"OK!\"}" 
+        return "{\"comment\" : \"OK!\"}"
     else:
         return "{\"comment\" : \"INVALID LOGIN, ERROR 404\"}"
-        
+
 
 @appx.route('/api/logout')
 def logoutapi():
@@ -301,7 +309,7 @@ def user(username):
     if username == "me":
         avatar = User.avatar(current_user.username)
         posts = db.postdb.find({"user_id": current_user._id})
-        return render_template('user.html', user=current_user,posts=posts,avatar=avatar)
+        return render_template('user.html', user=current_user, posts=posts, avatar=avatar)
     else:
         avatar = User.avatar(username)
         user = User.get_by_username(username)
@@ -311,7 +319,7 @@ def user(username):
             return redirect('/error/user')
         else:
             posts = db.postdb.find({"user_id": user._id})
-    return render_template('user.html',avatar=avatar, user=user,posts=posts,aboutme=aboutme)
+    return render_template('user.html', avatar=avatar, user=user, posts=posts, aboutme=aboutme)
 
 
 @appx.route("/createnewpost", methods=['GET', 'POST'])
@@ -323,12 +331,13 @@ def createnewpost2():
             title = request.form["title"]
             content = request.form["content"]
             user_id = current_user._id
-            new_post = Post(current_user.username, title, content,bruh.now().strftime('%Y-%m-%d %H:%M:%S')
-, user_id)
+            new_post = Post(current_user.username, title, content, bruh.now().strftime('%Y-%m-%d %H:%M:%S')
+                            , user_id)
             new_post.save_to_mongo()
             flash(f'Your post has been created!', 'success')
             return redirect('/me')
     return render_template('create_post.html', title='New Post', form=form)
+
 
 @appx.route("/deletepost", methods=['GET', 'POST'])
 @login_required
@@ -338,21 +347,20 @@ def deletepost():
     db.postdb.delete_one({"_id": post_id, "username": current_user.username})
     return redirect('/me')
 
+
 @appx.route("/set/aboutme", methods=['GET', 'POST'])
 @login_required
 def setaboutme():
     return redirect('/settings')
 
 
-
-
-@appx.route("/settings" , methods=['GET', 'POST'])
+@appx.route("/settings", methods=['GET', 'POST'])
 @login_required
 def settings():
     form = AboutMeForm()
     form.content.data = User.get_aboutme(current_user.username)
     form.email.data = current_user.email
-    form.content.data = User.get_aboutme(current_user.username)    
+    form.content.data = User.get_aboutme(current_user.username)
     if form.validate_on_submit():
         if request.method == 'POST':
             aboutme = request.form["content"]
@@ -364,6 +372,7 @@ def settings():
             flash(f'Your changes have been saved', 'success')
             return redirect('/me')
     return render_template('settings.html', title='Set About Me', form=form)
+
 
 @appx.errorhandler(404)
 def page_not_found(e):
@@ -380,7 +389,8 @@ def message(username):
             message = request.form["message"]
             h = '/sendmessage' + '?username=' + username + '&message=' + message
             return redirect(h)
-    return render_template('messages.html', chat=chat, username=username,form=hmm)
+    return render_template('messages.html', chat=chat, username=username, form=hmm)
+
 
 @appx.route("/sendmessage", methods=['GET', 'POST'])
 @login_required
@@ -392,8 +402,10 @@ def sendmessage():
     return redirect('/message/' + username)
 
 
+@appx.route("/dashboard/messaging")
+@login_required
+def dashboardmessaging():
+    return render_template('dashboard_messaging.html')
 
 
-
-
-appx.run(debug=True)
+appx.run(debug=True, host='localhost', port=5000)
