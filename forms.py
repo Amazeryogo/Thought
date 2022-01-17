da696d369532c539259c6ee0288dd860f23cb728
Suvid Datta <suvid.datta@gmail.com>
2022-01-17 12:39:27 +0530

    i think i should stop

--- forms.py
+++ forms.py
@@ -1,12 +1,14 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, TextAreaField,PasswordField, BooleanField, SubmitField
+from wtforms import StringField, TextAreaField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import DataRequired
+
 
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('Sign In')
+
 
 class CreateUserForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
@@ -15,19 +17,23 @@ class CreateUserForm(FlaskForm):
     invcode = StringField('Invite Code', validators=[DataRequired()])
     submit = SubmitField('Create User')
 
+
 class PostForm(FlaskForm):
     title = StringField('Title', validators=[DataRequired()])
     content = TextAreaField('Content', validators=[DataRequired()])
     submit = SubmitField('Post')
 
+
 class AboutMeForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired()])
-    content = TextAreaField('Content', validators=[DataRequired()])
+    content = TextAreaField('aboutme', validators=[DataRequired()])
     submit = SubmitField('Update')
+
 
 class ResetPasswordRequestForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired()])
     submit = SubmitField('Request Password Reset')
+
 
 class ResetPasswordForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
@@ -35,6 +41,7 @@ class ResetPasswordForm(FlaskForm):
         'Repeat Password', validators=[DataRequired()])
     submit = SubmitField('Please Reset my password')
 
+
 class MessageForm(FlaskForm):
     message = TextAreaField('Message', validators=[DataRequired()])
-    submit = SubmitField('📨')
+    submit = SubmitField('send')
